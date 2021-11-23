34c8e12f3b29554d73136c725c4e3fa205f36e94
Andrew <AND2181591@maricopa.edu>
2021-11-22 21:25:25 -0700

    Readded environment files for deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false, 
+  clientId: 'b1d52191336e4954a4f5518f6c68642b', 
+  clientSecret: '483d2c10efa74940bf5d1c294fe99097', 
+  firebaseConfig: {
+    apiKey: "AIzaSyDgtDEDhVJHEBumcIU_09MO3l68738fh0c",
+    authDomain: "trackmyrecords-b251a.firebaseapp.com",
+    projectId: "trackmyrecords-b251a",
+    storageBucket: "trackmyrecords-b251a.appspot.com",
+    messagingSenderId: "105159980272",
+    appId: "1:105159980272:web:4d792df971eefccd4cbe35",
+    measurementId: "G-SN064VQYRG"
+  }
+};
